890b02279c50a9a7aead9259acfd3b66035b7f5f
Kevin Kobori <kevinyujikobori@gmail.com>
2025-11-22 08:50:09 -0300

    1.55.0+1; feat: add damage handling to barrel decoration

--- lib/gameplay/decorations/interactables/barrel_decoration.dart
+++ lib/gameplay/decorations/interactables/barrel_decoration.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:bonfire/bonfire.dart';
+import 'package:darkness_dungeon/gameplay/characters/shared/character_fx_sprite_animations_config.dart';
+import 'package:darkness_dungeon/gameplay/core/modules/game/sprite_animation_config.dart';
 import 'package:darkness_dungeon/gameplay/core/modules/game/tile_constants.dart';
 import 'package:darkness_dungeon/gameplay/core/utils/hitbox_utils.dart';
 import 'package:darkness_dungeon/shared/framework/decorations/dd_pushable_decoration.dart';
@@ -19,18 +21,54 @@ final class _BarrelDecorationConfig {
     hitboxStartPositionX: 2.0,
     hitboxStartPositionY: 6.0,
   );
+
+  static Future<SpriteAnimation> _loadBreakAnimation() async {
+    try {
+      return await SpriteAnimation.load(
+        // 'gameplay/decorations/barrel_decoration_break_6.png',
+        'gameplay/decorations/barrel_decoration_AHUSHAU.png', // TODO(Kevin): creates barrel break sprites
+        SpriteAnimationConfig.createStandardData(
+          amount: 6,
+          textureSize: _textureSize,
+        ),
+      );
+    } catch (_) {
+      return CharacterFxSpriteAnimationsConfig.createExplosionRight7(); // TODO(Kevin): creates unique crash helper for these scenarios
+    }
+  }
 }
 
-class BarrelDecorationView extends DDPushableDecoration {
+class BarrelDecorationView extends DDPushableDecoration with Attackable {
   BarrelDecorationView({required super.position})
     : super.withSprite(
         sprite: _BarrelDecorationConfig._loadSprite(),
         size: _BarrelDecorationConfig._componentSize,
-      );
+      ) {
+    receivesAttackFrom = AcceptableAttackOriginEnum.PLAYER_AND_ALLY;
+  }
 
   @override
   Future<void> onLoad() {
     add(_BarrelDecorationConfig.createHitbox());
     return super.onLoad();
+  }
+
+  @override
+  void onReceiveDamage(AttackOriginEnum attacker, double damage, dynamic id) {
+    _playBreakAndRemove();
+  }
+
+  void _playBreakAndRemove() {
+    // Prevent multiple triggers
+    if (sprite == null) return;
+    playSpriteAnimationOnce(
+      _BarrelDecorationConfig._loadBreakAnimation(),
+      onStart: () {
+        sprite = null;
+      },
+      onFinish: () {
+        removeFromParent();
+      },
+    );
   }
 }
