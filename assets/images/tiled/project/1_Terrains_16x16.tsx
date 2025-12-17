6347181ed29fce4138efd15f4c2710deb33f652f
Kevin Kobori <kevinyujikobori@gmail.com>
2025-12-17 12:46:47 -0300

    1.64.15+1; config: add new tiled maps and tilesets from Modern Farm assets

---
+++ assets/images/tiled/project/1_Terrains_16x16.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="1_Terrains_16x16" tilewidth="16" tileheight="16" tilecount="736" columns="32">
+ <image source="../Modern_Farm_v1.2/16x16/tilesets/1_Terrains_16x16.png" width="512" height="368"/>
+ <tile id="83">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="203">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
