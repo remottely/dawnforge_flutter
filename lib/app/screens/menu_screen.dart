eb91d66273dfb5a88601f545f893eb849c7a4e47
Kevin Kobori <kevinyujikobori@gmail.com>
2026-01-03 14:25:45 -0300

    1.87.3+1; fix: adjust menu layout for better responsiveness

--- lib/app/screens/menu_screen.dart
+++ lib/app/screens/menu_screen.dart
@@ -30,28 +30,30 @@ class _MenuScreenState extends MenuScreenViewModel {
       backgroundColor: Colors.black,
       body: Center(
         child: SingleChildScrollView(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            spacing: DDDesignSystem.kSpacingLarge,
-            children: <Widget>[
-              const _Title(),
-              if (MenuScreenDef.characterSpriteAnimations.isNotEmpty) ...[
-                _CharacterAnimation(
-                  animation: MenuScreenDef
-                      .characterSpriteAnimations[currentCharacterSpriteIndex],
-                ),
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              spacing: DDDesignSystem.kSpacingLarge,
+              runSpacing: DDDesignSystem.kSpacingLarge,
+              children: <Widget>[
+                const _Title(),
+                if (MenuScreenDef.characterSpriteAnimations.isNotEmpty) ...[
+                  _CharacterAnimation(
+                    animation: MenuScreenDef
+                        .characterSpriteAnimations[currentCharacterSpriteIndex],
+                  ),
+                ],
+                _Controls(
+                  onControlMethodChanged: onControlMethodChanged,
+                ), // TODO(Kevin): NOW - put it back
+                switch (SettingsManager.instance.inputSelected) {
+                  InputActionsType.joystick =>
+                    const SizedBox.shrink(), // TODO(Kevin): Replace with joystick tip widget
+                  InputActionsType.keyboard => const _KeyboardTip(),
+                },
+                _StartButton(onPressed: navigateToGameplayScreen),
               ],
-              _Controls(
-                onControlMethodChanged: onControlMethodChanged,
-              ), // TODO(Kevin): NOW - put it back
-              switch (SettingsManager.instance.inputSelected) {
-                InputActionsType.joystick =>
-                  const SizedBox.shrink(), // TODO(Kevin): Replace with joystick tip widget
-                InputActionsType.keyboard => const _KeyboardTip(),
-              },
-              _StartButton(onPressed: navigateToGameplayScreen),
-            ],
-          ),
+            ),
         ),
       ),
       bottomNavigationBar: _Footer(onOpenURL: openExternalURL),
